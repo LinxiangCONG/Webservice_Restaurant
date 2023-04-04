bd98c341eff87e54e379066fb97793d1ec92d38d
yc <1005383957@qq.com>
2023-04-04 04:00:17 +0200

    modify soap project's strcture

---
+++ com/restaurant/service/src/main/java/com/restaurant/service/Service.java
@@ -0,0 +1,9 @@
+package com.restaurant.service;
+
+import javax.jws.WebService;
+
+@WebService(targetNamespace = "http://www.com.restaurant.service")
+public class Service {
+	
+	
+}
--- com/retaurant/web/src/main/java/com/restaurant/web/Service.java
+++
@@ -1,9 +0,0 @@
-package com.restaurant.web;
-
-import javax.jws.WebService;
-
-@WebService(targetNamespace = "http://www.com.restaurant.web")
-public class Service {
-	
-	
-}
