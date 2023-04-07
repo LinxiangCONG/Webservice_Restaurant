05d91cd82ce0423223ce481f5401eb421fe1fe92
LinxiangCONG <linxiangcong@outlook.com>
2023-04-07 12:05:49 +0200

    ajouter_restaurant.management.client

---
+++ restaurant.management.client/src/restaurant/management/client/RestClient.java
@@ -0,0 +1,41 @@
+package restaurant.management.client;
+
+import java.util.List;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import restaurant.management.web.data.Restaurant;
+
+public class RestClient {
+	private static final String REST_URI
+		= "http://localhost:8090/restaurant.management.web/api";
+	
+	private Client client = ClientBuilder.newClient();
+	
+	public Restaurant getRestaurantByName(String name) {
+		return client
+				.target(REST_URI)
+				.path("/retaurants/name/"+name)
+				.request(MediaType.APPLICATION_XML)
+				.get(Restaurant.class);	
+	}
+	
+	public List<Restaurant> getRestaurantsByCate(String cate) {
+		return client
+				.target(REST_URI)
+				.path("/categories/category/"+cate)
+				.request(MediaType.APPLICATION_XML)
+				.get(new GenericType<List<Restaurant>>() {});	
+	}
+	
+	public Response createXMLRestaurant(Restaurant restaurant) {
+		return client.target(REST_URI).path("/restaurants")
+				.request(MediaType.APPLICATION_XML)
+				.post(Entity.entity(restaurant, MediaType.APPLICATION_XML));
+	}
+}
